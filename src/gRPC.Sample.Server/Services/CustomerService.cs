133bacce11c2de555379e0d11748839bfaf9d4fb
mhd-ahmadi <mhd.ahmadi.dev@gmail.com>
2022-07-06 16:31:37 +0430

    Add new streaming channel

---
+++ src/gRPC.Sample.Server/Services/CustomerService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Sample.Server.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task GetNewCustomers(GetLookupRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation("Calling CustomerService...");
+            for (int i = 0; i < 1000; i++)
+            {
+                var newCustomer = new CustomerModel
+                {
+                    Id = i + 1,
+                    Age = Faker.RandomNumber.Next(18, 100),
+                    City = Faker.Address.City(),
+                    EmailAddress = Faker.Internet.Email(),
+                    FirstName = Faker.Name.First(),
+                    LastName = Faker.Name.Last(),
+                    Gender = Faker.Enum.Random<Gender>(),
+                };
+                await responseStream.WriteAsync(newCustomer);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
